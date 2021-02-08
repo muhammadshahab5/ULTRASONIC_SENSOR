dc997316a25250b3cf93e5ec19989343ed4bd0b1
muhammadshahab5 <77330005+muhammadshahab5@users.noreply.github.com>
2021-02-08 18:08:29 +0500

    Add files via upload

---
+++ ULTRASONIC_SKETCH/ULTRASONIC_SKETCH.ino
@@ -0,0 +1,25 @@
+                                        //ULTRASONIC///
+int trigger_pin = 2;
+int echo_pin = 3;
+int buzzer_pin = 10; 
+int time;
+int distance; 
+
+void setup ( ) {
+
+Serial.begin (9600); 
+
+pinMode (trigger_pin, OUTPUT); 
+pinMode (echo_pin, INPUT);
+pinMode (buzzer_pin, OUTPUT);
+
+}
+
+void loop ( ) {
+
+digitalWrite (trigger_pin, HIGH);
+delay(10);
+digitalWrite (trigger_pin, LOW);
+time = pulseIn (echo_pin, HIGH);
+
+ distance = (time * 0.034) / 2;
